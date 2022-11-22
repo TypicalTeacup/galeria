75dde38c42866fceaa3008730a67f171713337f0
Kacper Skirel <pskirel@wp.pl>
2022-11-22 18:11:08 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let intervalHandle;
+let currentImg = 0;
+let interval = 5000;
+
+let mainImg = document.getElementById("main");
+let gallery = document.getElementById("galeria");
+
+let images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+
+function change(img) {
+    currentImg = img === undefined ? (currentImg + 1) % 4 : img - 1;
+    console.log(currentImg);
+    mainImg.src = images[currentImg];
+    intervalHandle =
+        img == 0
+            ? intervalHandle
+            : clearInterval(intervalHandle) === undefined
+            ? setInterval(change, interval)
+            : null;
+    console.log(intervalHandle);
+}
+
+fetch("https://www.placecage.com/c/53/135/", { mode: "no-cors", cache: "no-cache" }).then(
+    () => {
+        //change photos to random
+        images = [];
+        for (let i = 0; i < gallery.children.length; i++) {
+            const image = gallery.children.item(i);
+            console.log(image);
+            let width = Math.floor(Math.random() * 40 + 10);
+            let height = Math.floor(Math.random() * 40 + 10);
+            let url = `https://www.placecage.com/c/${width}0/${height}0`;
+            images.push(`https://www.placecage.com/c/${width}0/${height}0`);
+            image.src = url;
+        }
+    }
+).catch(() =>{
+    images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+});
+
+intervalHandle = setInterval(change, interval);
